1eefc5d08b03710baa8b1b9f2d3b30265e8ed581
neoki <ot07neoki@gmail.com>
2024-08-14 15:17:09 +0900

    feat: add logout endpoint

--- src/index.ts
+++ src/index.ts
@@ -10,9 +10,56 @@ const app = new Hono<{ Bindings: Bindings }>();
 
 app.use("/auth/github/login", githubAuth({}));
 
-app.get("/auth/github/login", async (c) => {
-  const oldSessionId = getCookie(c, "session_id");
-  if (oldSessionId) {
+app
+  .get("/auth/github/login", async (c) => {
+    const oldSessionId = getCookie(c, "session_id");
+    if (oldSessionId) {
+      setCookie(c, "session_id", "", {
+        httpOnly: true,
+        secure: true,
+        sameSite: "None",
+        maxAge: 0,
+      });
+
+      await c.env.SESSION_STORE_KV.delete(oldSessionId);
+    }
+
+    const accessToken = c.get("token");
+    const refreshToken = c.get("refresh-token");
+    const user = c.get("user-github");
+
+    if (!user) {
+      return c.json({ error: "User not found" }, 401);
+    }
+
+    const ttl = 60 * 60 * 24;
+    const sessionId = crypto.randomUUID();
+    setCookie(c, "session_id", sessionId, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "None",
+      maxAge: ttl,
+      path: "/",
+    });
+
+    const session = {
+      user,
+      accessToken,
+      refreshToken,
+    };
+
+    await c.env.SESSION_STORE_KV.put(sessionId, JSON.stringify(session), {
+      expirationTtl: ttl,
+    });
+
+    return c.text("Successfully logged in");
+  })
+  .get("/auth/logout", async (c) => {
+    const session = getCookie(c, "session_id");
+    if (!session) {
+      return c.text("Not logged in", 401);
+    }
+
     setCookie(c, "session_id", "", {
       httpOnly: true,
       secure: true,
@@ -20,38 +67,9 @@ app.get("/auth/github/login", async (c) => {
       maxAge: 0,
     });
 
-    await c.env.SESSION_STORE_KV.delete(oldSessionId);
-  }
+    await c.env.SESSION_STORE_KV.delete(session);
 
-  const accessToken = c.get("token");
-  const refreshToken = c.get("refresh-token");
-  const user = c.get("user-github");
-
-  if (!user) {
-    return c.json({ error: "User not found" }, 401);
-  }
-
-  const ttl = 60 * 60 * 24;
-  const sessionId = crypto.randomUUID();
-  setCookie(c, "session_id", sessionId, {
-    httpOnly: true,
-    secure: true,
-    sameSite: "None",
-    maxAge: ttl,
-    path: "/",
+    return c.text("Successfully logged out");
   });
-
-  const session = {
-    user,
-    accessToken,
-    refreshToken,
-  };
-
-  await c.env.SESSION_STORE_KV.put(sessionId, JSON.stringify(session), {
-    expirationTtl: ttl,
-  });
-
-  return c.text("Successfully logged in");
-});
 
 export default app;
